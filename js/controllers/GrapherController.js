f819df7e9d098c5e928e1f8df09573f78cb11ca6
jhumigas <jhumigas@gmail.com>
2015-08-14 11:54:28 +0200

    $timeout to set

--- js/controllers/GrapherController.js
+++ js/controllers/GrapherController.js
@@ -1,21 +1,31 @@
-app.controller('GrapherController',['$scope' ,function ($scope) {
+app.controller('GrapherController',['$scope','$timeout' ,function ($scope,$timeout) {
   $scope.portofolio=[
   {
     link: '#portfolioModal1',
     imgsrc: 'img/portfolio/cabin.png',
-    template: 'a.html'
+    template: 'a.html',
+    visibility: false
   },
   {
     link: '#portfolioModal3',
     imgsrc: 'img/portfolio/circus.png',
-    template: 'c.html'
+    template: 'c.html',
+    visibility: false
   },
   {
     link: '#portfolioModal2',
     imgsrc: 'img/portfolio/cake.png',
-    template: 'b.html'
+    template: 'b.html',
+    visibility: false
   }
   ];
   $scope.date = new Date();
+  var initialisation = function(){
+    for(var item in $scope.portofolio){
+      $scope.portofolio[item].visibility = true;
+    }
+  }
+  $timeout(initialisation,500)
     },
+
 ]);
